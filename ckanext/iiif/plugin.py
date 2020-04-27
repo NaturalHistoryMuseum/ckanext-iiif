44e6779a225e391023990ddecc7d59c103afe52b
Josh Humphries <j.humphries@nhm.ac.uk>
2020-04-27 23:59:59 +0100

    Add IVersionedDatastore hook for iiif record augmentation
    Using the new datastore_multisearch_modify_response hook we add a iiif
    key to the root of the record and build the manifest straight into it.
    This is currently only implemented for the vfactor iiif work,
    generification to come.
    
    Doing this is useful when a user is trying to get the manifests for
    multiple records as calling the manifest endpoint for each record is
    inefficient.

--- ckanext/iiif/plugin.py
+++ ckanext/iiif/plugin.py
@@ -1,16 +1,24 @@
 import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+from ckan.plugins import toolkit
+from contextlib2 import suppress
 
 from . import interfaces
 from . import routes
-from .routes import iiif
 from .lib.manifest import IIIFRecordManifestBuilder
+from .routes import iiif
 
 
 class IIIFPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IBlueprint)
     plugins.implements(plugins.IConfigurable)
     plugins.implements(interfaces.IIIIF)
+
+    try:
+        # hook if we can
+        from ckanext.versioned_datastore.interfaces import IVersionedDatastore
+        plugins.implements(IVersionedDatastore, inherit=True)
+    except ImportError:
+        pass
 
     # IIIIF
     def get_builders(self):
@@ -23,11 +31,28 @@ class IIIFPlugin(plugins.SingletonPlugin):
         for plugin in plugins.PluginImplementations(interfaces.IIIIF):
             for builder in plugin.get_builders():
                 if builder.regex in iiif.builders:
-                    continue
-                    # TODO: this
-                    # raise Exception(u'Duplicate IIIF builder regex: {}'.format(regex.pattern))
+                    raise Exception(u'Duplicate IIIF builder regex: {}'.format(
+                        builder.regex.pattern))
                 iiif.builders[builder.regex] = builder.get_builder
 
     # IBlueprint
     def get_blueprint(self):
         return routes.blueprints
+
+    # IVersionedDatastore
+    def datastore_multisearch_modify_response(self, response):
+        resource_cache = {}
+        resource_show = toolkit.get_action(u'resource_show')
+        vfactor_resource_id = toolkit.config.get(u'ckanext.nhm.vfactor_resource_id')
+
+        for record in response[u'records']:
+            resource_id = record[u'resource']
+            if resource_id == vfactor_resource_id:
+                if resource_id not in resource_cache:
+                    resource_cache[resource_id] = resource_show({}, {u'id': resource_id})
+                with suppress(Exception):
+                    builder = IIIFRecordManifestBuilder(resource_cache[resource_id],
+                                                        record[u'data'])
+                    record[u'iiif'] = builder.build()
+
+        return response
