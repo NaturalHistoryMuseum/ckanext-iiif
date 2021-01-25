6e24b395a612163e2fa64b78872c106af1faed75
Josh Humphries <j.humphries@nhm.ac.uk>
2021-01-25 16:46:26 +0000

    Python3 support

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,18 @@
 import pytest
-from ckanext.iiif.lib.utils import wrap_language, create_image_server_url
 from mock import MagicMock
 
+from ckanext.iiif.lib.utils import wrap_language, create_image_server_url
 
-@pytest.mark.ckan_config(u'ckanext.iiif.image_server_url', u'https://something.com')
+
+@pytest.mark.ckan_config('ckanext.iiif.image_server_url', 'https://something.com')
 def test_create_image_server_url():
-    result = create_image_server_url(u'an_identifier!', identifier_type=u'a_type!')
-    assert result == u'https://something.com/a_type!:an_identifier!'
+    result = create_image_server_url('an_identifier!', identifier_type='a_type!')
+    assert result == 'https://something.com/a_type!:an_identifier!'
 
 
 def test_wrap_language():
     test_value = MagicMock()
 
-    assert wrap_language(test_value) == {u'en': [test_value]}
-    assert wrap_language([test_value]) == {u'en': [test_value]}
-    assert wrap_language(test_value, language=u'beans') == {u'beans': [test_value]}
+    assert wrap_language(test_value) == {'en': [test_value]}
+    assert wrap_language([test_value]) == {'en': [test_value]}
+    assert wrap_language(test_value, language='beans') == {'beans': [test_value]}
