3fba784e48c8b0d9acf86624a610325f5414dae0
Josh Humphries <j.humphries@nhm.ac.uk>
2022-10-12 21:54:06 +0100

    Fix tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,5 @@
-import pytest
+from ckanext.iiif.lib.utils import wrap_language
 from mock import MagicMock
-
-from ckanext.iiif.lib.utils import wrap_language, create_image_server_url
-
-
-@pytest.mark.ckan_config('ckanext.iiif.image_server_url', 'https://something.com')
-def test_create_image_server_url():
-    result = create_image_server_url('an_identifier!', identifier_type='a_type!')
-    assert result == 'https://something.com/a_type!:an_identifier!'
 
 
 def test_wrap_language():
